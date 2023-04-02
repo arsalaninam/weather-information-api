ac66eff401e8f4702cde421b51e9d8e15a1c5862
Arsalan Inam <m.arsalaninam@gmail.com>
2023-04-02 04:33:34 +0200

    Add redis configurations

---
+++ weather-information-api/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.example.demo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.headers().cacheControl().disable()
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/api/weather").hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET, "/api/weather").hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET, "/api/weather/error").hasRole("ADMIN")
+                .antMatchers(HttpMethod.POST, "/weather").hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET, "/weather").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .csrf().disable();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password(passwordEncoder().encode("admin")).roles("ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
