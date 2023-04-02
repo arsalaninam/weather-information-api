d576ffa74f4c9f75f8cba6a3f127346d953f3f44
Arsalan Inam <m.arsalaninam@gmail.com>
2023-04-02 21:42:26 +0200

    Add project constants

---
+++ weather-information-data-api-test-java/src/main/java/org/dcs/testcase/constant/ScenarioNameConstant.java
@@ -0,0 +1,12 @@
+package org.dcs.testcase.constant;
+
+public interface ScenarioNameConstant {
+
+    String VALIDATE_STATUS_CODE_200 =
+            "Validate Response Status Code is 200 : ";
+
+    String CRETE_WEATHER_INFO_BY_POST_REQUEST = "Create a weather information by Post request : ";
+    String VALIDATE_SUCCESS_RESPONSE_BODY = "Validate Success Response Body of API : ";
+
+    String VALIDATE_ERROR_RESPONSE_BODY = "Validate Error Response Body of API : ";
+}
---
+++ weather-information-data-api-test-java/src/main/java/org/dcs/testcase/constant/ServiceConstant.java
@@ -0,0 +1,19 @@
+package org.dcs.testcase.constant;
+
+public interface ServiceConstant {
+
+    String BASE_URL = "baseURL";
+
+    String USERNAME = "username";
+
+    String PASSWORD = "password";
+
+    String WEATHER_GET_URL_BY_DATE_ENDPOINT = "getWeatherApiByDateEndpoint";
+
+    String WEATHER_GET_URL_BY_CITY_ENDPOINT = "getWeatherApiByCityEndpoint";
+
+    String WEATHER_POST_URL = "weatherApiPostEndpoint";
+
+    String WEATHER_ERROR_URL = "weatherApiErrorEndpoint";
+
+}
