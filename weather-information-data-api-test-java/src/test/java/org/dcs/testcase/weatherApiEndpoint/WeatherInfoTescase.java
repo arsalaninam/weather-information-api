d27d89be01b0980c66479a73def6d70584ca9286
Arsalan Inam <m.arsalaninam@gmail.com>
2023-04-02 21:45:51 +0200

    Add weather api test cases

---
+++ weather-information-data-api-test-java/src/test/java/org/dcs/testcase/weatherApiEndpoint/WeatherInfoTescase.java
@@ -0,0 +1,94 @@
+package org.dcs.testcase.weatherApiEndpoint;
+
+import org.dcs.testcase.TestBase;
+import org.dcs.testcase.businesslayer.weather.WeatherApiBusinessLogic;
+import org.dcs.testcase.data.weather.GetWeatherInfoByCityDataProvider;
+import org.dcs.testcase.data.weather.GetWeatherInfoByDateDataProvider;
+import org.dcs.testcase.data.weather.PostWeatherInfoDataProvider;
+import org.dcs.testcase.pogo.weather.ListWeatherResponse;
+import org.dcs.testcase.pogo.weather.WeatherApiResponse;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+import static org.dcs.testcase.constant.ScenarioNameConstant.CRETE_WEATHER_INFO_BY_POST_REQUEST;
+import static org.dcs.testcase.constant.ScenarioNameConstant.VALIDATE_SUCCESS_RESPONSE_BODY;
+import static org.dcs.testcase.constant.ServiceConstant.*;
+
+public class WeatherInfoTescase extends TestBase {
+    private final String baseUrl = prop.getProperty(BASE_URL);
+    private final String username = prop.getProperty(USERNAME);
+    private final String password = prop.getProperty(PASSWORD);
+    private final String getGetWeatherByDateEndpoint = prop.getProperty(WEATHER_GET_URL_BY_DATE_ENDPOINT);
+    private final String getWeatherByDateUrl = baseUrl + getGetWeatherByDateEndpoint;
+    private static final Logger log = LoggerFactory.getLogger(WeatherInfoTescase.class);
+
+    /*****************************************************************************
+     * Send a Post request to /api/weather and Validate that response
+     * returns the expected status code 200 and successful response
+     *****************************************************************************/
+    @Test(dataProvider = "postWeatherInfo",
+            dataProviderClass = PostWeatherInfoDataProvider.class)
+    public void testPostResponseToCreateWeatherInfoByDate(String date, String city,
+                                                          String temperature, String humidity) {
+        log.info(CRETE_WEATHER_INFO_BY_POST_REQUEST + getWeatherByDateUrl);
+        JSONObject weatherInfoRequestBody = new JSONObject();
+        weatherInfoRequestBody.put("date", date);
+        weatherInfoRequestBody.put("city", city);
+        weatherInfoRequestBody.put("temperature", temperature);
+        weatherInfoRequestBody.put("humidity", humidity);
+
+        String weatherApiResponse = WeatherApiBusinessLogic.postWeatherInformation(weatherInfoRequestBody, username, password);
+        Assert.assertEquals(weatherApiResponse, "Weather data added successfully.");
+    }
+
+    /*****************************************************************************
+     * Send a Get request to /api/weather?date= and Validate that response
+     * returns the expected date, city, temperature and humidity
+     *****************************************************************************/
+    @Test(dataProvider = "getWeatherInfoByDate",
+            dataProviderClass = GetWeatherInfoByDateDataProvider.class)
+    public void testGetResponseByDateFetchesCorrectResponse(String date, String city,
+                                                            int temperature, int humidity) {
+        log.info(VALIDATE_SUCCESS_RESPONSE_BODY + getWeatherByDateUrl);
+        ListWeatherResponse weatherApiResponseList = WeatherApiBusinessLogic.getWeatherResponseByDate(date, username, password);
+        List<WeatherApiResponse> allWeatherInfoList = weatherApiResponseList.getWeatherApiResponseList();
+        System.out.println(weatherApiResponseList.getWeatherApiResponseList());
+
+        for (WeatherApiResponse weatherApiResponse : allWeatherInfoList) {
+            System.out.println(weatherApiResponse.getDate() + date);
+            softAssert.assertEquals(weatherApiResponse.getDate(), date);
+            softAssert.assertEquals(weatherApiResponse.getCity(), city);
+            softAssert.assertEquals(weatherApiResponse.getTemperature(), temperature);
+            softAssert.assertEquals(weatherApiResponse.getHumidity(), humidity);
+            softAssert.assertAll();
+        }
+    }
+
+    /*****************************************************************************
+     * Send a Get request to /api/weather?city= and Validate that response
+     * returns the expected date, city, temperature and humidity
+     *****************************************************************************/
+    @Test(dataProvider = "getWeatherInfoByCity",
+            dataProviderClass = GetWeatherInfoByCityDataProvider.class)
+    public void testGetResponseByCityFetchesCorrectResponse(String date, String city,
+                                                            int temperature, int humidity) {
+        log.info(VALIDATE_SUCCESS_RESPONSE_BODY + getWeatherByDateUrl);
+        ListWeatherResponse weatherApiResponseList = WeatherApiBusinessLogic.getWeatherResponseByCity(city, username, password);
+        List<WeatherApiResponse> allWeatherInfoList = weatherApiResponseList.getWeatherApiResponseList();
+        System.out.println(weatherApiResponseList.getWeatherApiResponseList());
+
+        for (WeatherApiResponse weatherApiResponse : allWeatherInfoList) {
+            System.out.println(weatherApiResponse.getDate() + date);
+            softAssert.assertEquals(weatherApiResponse.getDate(), date);
+            softAssert.assertEquals(weatherApiResponse.getCity(), city);
+            softAssert.assertEquals(weatherApiResponse.getTemperature(), temperature);
+            softAssert.assertEquals(weatherApiResponse.getHumidity(), humidity);
+            softAssert.assertAll();
+        }
+    }
+}
